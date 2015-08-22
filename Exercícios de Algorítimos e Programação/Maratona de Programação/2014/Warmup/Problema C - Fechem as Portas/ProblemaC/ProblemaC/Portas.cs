81347ee56ea6b05e0133c2bf63c037e2286fc1e5
paulostradioti <paulo@advus.com>
2015-08-22 14:58:13 -0300

    Problema C - Fechem as Portas

---
+++ Exercícios de Algorítimos e Programação/Maratona de Programação/2014/Warmup/Problema C - Fechem as Portas/ProblemaC/ProblemaC/Portas.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemaC
+{
+    class Portas
+    {
+        static void Main(string[] args)
+        {
+            List<int> portasAbertas = new List<int>();
+            List<int> descendentesDentroMansao = new List<int>();
+            int numDescendentes;
+            string userInput;
+                        
+            userInput = Console.ReadLine();
+            Int32.TryParse(userInput, out numDescendentes);
+            if (numDescendentes < 1)
+            {
+                return;
+            }
+            
+            
+            Random random = new Random();
+            while (descendentesDentroMansao.Count() != numDescendentes)
+            {
+                int descendente = 0;
+                do
+	            {
+                    descendente = random.Next(1, numDescendentes + 1); // upper bound is excluse in Random.Next
+	            } while(descendentesDentroMansao.Contains(descendente));
+                
+                descendentesDentroMansao.Add(descendente);
+
+                var multiplicador = 1;
+                var porta = descendente;
+                do
+                {
+
+                    if (portasAbertas.Contains(porta))
+                    {
+                        portasAbertas.Remove(porta);
+                    }
+                    else
+                    {
+                        portasAbertas.Add(porta);
+                    }
+
+                    multiplicador++;
+                    porta = descendente * multiplicador;
+
+                } while (porta <= numDescendentes);
+
+            }
+
+            portasAbertas.Sort();
+
+            Console.WriteLine(String.Join(" ", portasAbertas.Select(x => Convert.ToString(x))));
+        }
+    }
+
+}
